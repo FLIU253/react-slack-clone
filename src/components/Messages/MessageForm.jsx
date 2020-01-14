7c525d3f475c5db30c7ce277303aaa64c362f6da
Franklin Liu <franklin.liu@autodata.net>
2020-01-14 13:24:58 -0500

    message adding functionality partially implemented

---
+++ src/components/Messages/MessageForm.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Segment, Button, Input } from "semantic-ui-react";
+import firebase from "../../firebase";
+
+const MessageForm = ({ messagesRef, currentChannel, currentUser }) => {
+  const [state, setState] = useState({
+    message: "",
+    loading: false,
+    channel: currentChannel,
+    user: currentUser,
+    errors: ""
+  });
+
+  const handleChange = event => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
+
+  const sendMessage = () => {
+    if (state.message) {
+      setState({ ...state, loading: true });
+      messagesRef
+        .child(state.channel.id)
+        .push()
+        .set(createMessage())
+        .then(() => {
+          setState({ ...state, loading: false, message: "" }).catch(err => {
+            console.error(err);
+            setState({
+              ...state,
+              loading: false,
+              errors: err
+            });
+          });
+        });
+    } else {
+      setState({
+        ...state,
+        errors: "Add a message"
+      });
+    }
+  };
+
+  const createMessage = () => {
+    const message = {
+      content: state.message,
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: state.user.uid,
+        name: state.user.displayName,
+        avatar: state.user.photoURL
+      }
+    };
+    return message;
+  };
+
+  return (
+    <Segment className="message__form">
+      <Input
+        fluid
+        name="message"
+        style={{ marginBottom: "0.7em" }}
+        label={<Button icon={"add"} />}
+        labelPosition="left"
+        placeholder="Write your message"
+        className={state.errors.length > 1 ? "error" : ""}
+        onChange={event => handleChange(event)}
+      />
+      <Button.Group icon widths="2">
+        <Button
+          onClick={sendMessage}
+          color="orange"
+          content="Add Reply"
+          labelPosition="left"
+          icon="edit"
+        />
+        <Button
+          color="teal"
+          content="Upload Media"
+          labelPosition="right"
+          icon="cloud upload"
+        />
+      </Button.Group>
+    </Segment>
+  );
+};
+
+export default MessageForm;
--- src/components/Messages/Messages.jsx
+++ src/components/Messages/Messages.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
+import firebase from "../../firebase";
 
-const Messages = () => {
-  return <div>Messages</div>;
+const Messages = ({ currentChannel, currentUser }) => {
+  const [state, setState] = useState({
+    messagesRef: firebase.database().ref("messages"),
+    channel: currentChannel,
+    user: currentUser
+  });
+
+  return (
+    <Fragment>
+      <MessagesHeader />
+
+      <Segment>
+        <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+      </Segment>
+
+      <MessageForm
+        messagesRef={state.messagesRef}
+        currentChannel={state.channel}
+        currentUser={state.user}
+      />
+    </Fragment>
+  );
 };
 
 export default Messages;
