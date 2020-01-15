15aac705f4b0e4feae4f8c8b408451559ca061f6
Franklin Liu <franklin.liu@autodata.net>
2020-01-15 15:19:03 -0500

    top posts added

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetalPanel from "./MetalPanel/MetalPanel";
 
-const App = ({ currentUser, currentChannel, isPrivateChannel }) => {
+const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts }) => {
   return (
     <Grid columns="equal" className="app" style={{ background: "#eee" }}>
       <ColorPanel />
@@ -27,7 +27,12 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => {
       </Grid.Column>
 
       <Grid.Column width={4}>
-        <MetalPanel />
+        <MetalPanel
+          isPrivateChannel={isPrivateChannel}
+          key={currentChannel && currentChannel.id}
+          userPosts={userPosts}
+          currentChannel={currentChannel}
+        />
       </Grid.Column>
     </Grid>
   );
@@ -36,7 +41,8 @@ const App = ({ currentUser, currentChannel, isPrivateChannel }) => {
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
-  isPrivateChannel: state.channel.isPrivateChannel
+  isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts
 });
 
 export default connect(mapStateToProps)(App);
